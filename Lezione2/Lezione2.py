63f3fb1dc61c3e34528cd2e02e0c9b8eed1df93b
talpaa <daniele.taccini@gmail.com>
2024-04-18 12:28:01 +0200

    primo programma in lezione2

---
+++ Lezione2/Lezione2.py
@@ -0,0 +1,9 @@
+#Daniele Taccini
+
+#questa variabile contiene il nome
+nome: str = "Erik"
+
+#questa variabile contiene il messaggio
+message: str = f"Hello", nome, ", ti va di imparare python isieme?"
+
+print(message)
