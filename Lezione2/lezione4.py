7b3e7d8b752ca2449d1c8ad944bc0e0734f2fc07
talpaa <daniele.taccini@gmail.com>
2024-04-19 13:07:30 +0200

    lesson4.py esercizio 1

--- Lezione2/lezione4.py
+++ Lezione2/lezione4.py
@@ -1,3 +1,8 @@
+"""Let’s try to define a function named subtract ourselves:
+- It should take 2 parameters.
+- Inside the function, it should subtract the two.
+- Then, return the result."""
+
 def sottrazione(a:int,b:int) -> int:
 
     sott = a - b
@@ -6,3 +11,30 @@ def sottrazione(a:int,b:int) -> int:
 
 risultato = sottrazione(434,276)
 print(risultato)
+
+
+
+"""
+Write a function check_value(), which takes a number as an argument.
+Using if / else, the function should print whether the number is bigger, smaller,
+or equal to the other number.
+"""
+
+
+
+def check_value(num,check):
+
+    if num > check:
+        print(f"{num} è più grande di {check}")
+
+    elif num==check:
+        print(f"{num} è uguale a {check}")
+
+    else:
+        print(f"{num} è più piccolo di {check}")
+
+
+#num = input("Inserisci un numero e io ti dirò se è piu grande, uguale o minore del numero : ")
+
+
+check_value(7,1)
