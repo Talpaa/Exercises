cc8d31d15a0c05eb684385b4f992ff25370d8d4d
talpaa <daniele.taccini@gmail.com>
2024-04-19 13:46:50 +0200

    lezione2.py 3-6

--- Lezione2/Lezione2.py
+++ Lezione2/Lezione2.py
@@ -109,10 +109,16 @@ for i in list_vehicles:
 
 #3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. 
 #You’ll have to think of someone else to invite.
-
 # Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
 # Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
 # Print a second set of invitation messages, one for each person who is still in your list.
+
+#3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+# Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+# Use insert() to add one new guest to the beginning of your list.
+# Use insert() to add one new guest to the middle of your list.
+# Use append() to add one new guest to the end of your list.
+# Print a new set of invitation messages, one for each person in your list.
 
 
 def manda_inviti(list_invited):
@@ -136,6 +142,21 @@ list_invited[2] = "persona5"
 
 #richiamo la funzione che ho creato per mandare gli inviti
 manda_inviti(list_invited)
+
+print("\n\nCiao invitati, vi scrivo per informarvi di aver trovato un tavolo più grande")
+
+
+middle = (len(list_invited))//2
+list_invited.insert(middle,"persona7")
+list_invited.insert(0,"persona0")
+list_invited.append("persona6")
+
+#richiamo la funzione che ho creato per mandare gli inviti
+manda_inviti(list_invited)
+
+
+
+
 
 
 
