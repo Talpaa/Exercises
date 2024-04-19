05e007a5c877c0853962e7e619f3b72b0a2848f8
talpaa <daniele.taccini@gmail.com>
2024-04-19 13:21:23 +0200

    lexione4.py esercizio 2

--- Lezione2/lezione4.py
+++ Lezione2/lezione4.py
@@ -20,8 +20,6 @@ Using if / else, the function should print whether the number is bigger, smaller
 or equal to the other number.
 """
 
-
-
 def check_value(num,check):
 
     if num > check:
@@ -33,8 +31,31 @@ def check_value(num,check):
     else:
         print(f"{num} è più piccolo di {check}")
 
-
 #num = input("Inserisci un numero e io ti dirò se è piu grande, uguale o minore del numero : ")
-
-
 check_value(7,1)
+
+
+
+
+"""
+Write a function check_length(), which takes a string as an argument.
+Using if / else, check if the length of the string is bigger, smaller, or equal to 10
+characters."""
+
+def check_length(stringa):
+
+    len_str = len(stringa)
+
+    if len_str > 10:
+        print(f"\"{stringa}\" è più lunga di 10 caratteri")
+
+    elif len_str == 10:
+        print(f"\"{stringa}\" è lunga 10 caratteri")
+
+    else:
+        print(f"\"{stringa}\" è più corta di 10 caratteri")
+
+
+stringa = input("inserisci una stringa a piacere e io ti diro se è più lunga, uguale o più corta di 10 caratteri: \n")
+
+check_length(stringa)
