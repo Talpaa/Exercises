f9f795ffaffc9530d3ce4f7fa1f597eeddf5e65f
talpaa <daniele.taccini@gmail.com>
2024-04-22 17:01:27 +0200

    lezione4-bis esercizio 2

--- lezione4/lezione4-bis.py
+++ lezione4/lezione4-bis.py
@@ -1,3 +1,6 @@
+
+#esercizio 1
+
 def palindromi(x):
 
     str_reverse = x[::-1]    
@@ -22,3 +25,40 @@ str_y = str(y)
 palindromi(str_x)
 
 palindromi(str_y)
+
+#esercizio 2
+
+def ultima_parola(s):
+
+    a: int = 0
+    b: int = 0
+
+    for i in s[::-1]:
+
+        if i != " ":
+
+            a += 1
+
+        elif(i == " ")and(a != 0):
+
+            return a
+
+    
+
+
+
+
+
+s1: str = "Hello world     "
+
+ultima = ultima_parola(s1)
+print(f"\n\nL'ultima parola della frase è lunga {ultima}")
+
+s2: str = "fly me to the moon"
+ultima = ultima_parola(s2)
+print(f"\nL'ultima parola della frase è lunga {ultima}")
+
+s3: str = "lufy is still joyboy"
+ultima = ultima_parola(s3)
+print(f"\nL'ultima parola della frase è lunga {ultima}")
+
