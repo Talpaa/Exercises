a453f043ed8a47c9e9433812bff5de5d9b57b8d4
Talpaa <daniele.taccini@gmail.com>
2024-04-27 18:05:09 +0200

    lezione2 esercizio 3-6

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -120,17 +120,47 @@ for i in list_vehicles:
 # Use append() to add one new guest to the end of your list.
 # Print a new set of invitation messages, one for each person in your list.
 
+#3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and now you have space for only two guests.
+# Start with your program from Exercise 3-6. Add a new line that prints a message saying that you can invite only two people for dinner.
+# Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, 
+#print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+# Print a message to each of the two people still on your list, letting them know they’re still invited.
+# Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
 
-def manda_inviti(list_invited):
+
+def manda_inviti(list_invited: list):
 
     #ciclo per scrivere l'invito per ogni nome presente nella lista
     for i in list_invited:
 
         print(f"\nCiao {i}, mi piacerebbe invitarti a cena.")
 
+    
+
+def ritira_inviti(list_invited: list)->list:
+
+    i: int = (len(list_invited))-1
+
+    while(len(list_invited) > 2):
+
+        print(f"\n{list_invited[i]}, sono dispiaciuto di dover annulare la cena organizata")
+        list_invited.pop(i)
+        i = (len(list_invited))-1
+
+    print("\n")
+    for j in list_invited:
+
+        print(f"\n{j}, sei ancora invitato alla cena organizzata")
+
+    del list_invited
+
+    #return list_invited
+
+    
+
 
 #creazione lista invitati
-list_invited: str = ["persona1","persona2","persona3","persona4"]
+list_invited: list = ["persona1","persona2","persona3","persona4"]
 
 #chiamo la funzione che ho creato per mandare gli inviti
 manda_inviti(list_invited)
@@ -145,7 +175,7 @@ manda_inviti(list_invited)
 
 print("\n\nCiao invitati, vi scrivo per informarvi di aver trovato un tavolo più grande")
 
-
+#serve a trovare il centro della lista per inserire un nuovo invitato
 middle = (len(list_invited))//2
 list_invited.insert(middle,"persona7")
 list_invited.insert(0,"persona0")
@@ -153,6 +183,11 @@ list_invited.append("persona6")
 
 #richiamo la funzione che ho creato per mandare gli inviti
 manda_inviti(list_invited)
+
+print("\n\n\n")
+list_invited = ritira_inviti(list_invited)
+
+print(f"\n{list_invited}")
 
 
 
