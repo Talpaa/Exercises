1cc5b3f1a3f3bec3d8c3b493f2d020aa0575082b
Talpaa <daniele.taccini@gmail.com>
2024-04-27 18:30:42 +0200

    lezione2 esercizio 3-8

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -191,6 +191,44 @@ print(f"\n{list_invited}")
 
 
 
+#3-8. Seeing the World: Think of at least five places in the world you’d like to visit.
+# Store the locations in a list. Make sure the list is not in alphabetical order.
+place: list = ["Tenerife","Florida","Giappone","Islanda","Marocco"]
+
+# Print your list in its original order. Don’t worry about printing the list neatly; just print it as a raw Python list.
+print(f"{place}")
+
+# Use sorted() to print your list in alphabetical order without modifying the actual list.
+print(f"{sorted(place)}")
+
+# Show that your list is still in its original order by printing it.
+print(f"{place}")
+
+# Use sorted() to print your list in reverse-alphabetical order without changing the order of the original list.
+print(f"{sorted(place, reverse=True)}")
+
+# Show that your list is still in its original order by printing it again.
+print(f"{place}")
+
+# Use reverse()  to change the order of your list. Print the list to show that its order has changed.
+place.reverse()
+print(f"{place}")
+
+# Use reverse() to change the order of your list again. Print the list to show it’s back to its original order.
+place.reverse()
+print(f"{place}")
+
+# Use sort() to change your list so it’s stored in alphabetical order. Print the list to show that its order has been changed.
+place.sort()
+print(f"{place}")
+# Use sort() to change your list so it’s stored in reverse-alphabetical order. Print the list to show that its order has changed.
+place.sort(reverse=True)
+print(f"{place}")
+
+
+
+
+
 
 
 
