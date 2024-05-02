1dfa4f8a32886e1cc374860a1c736eee77749429
talpaa <daniele.taccini@gmail.com>
2024-05-02 13:16:55 +0200

    lezione5 creazione file personal_info.py

---
+++ lezione5/personal_info.py
@@ -0,0 +1,6 @@
+#3. Personal Info:
+
+"""write a build_profile() function that accepts the name , surname,  occupation, location, and age  of a person. 
+    Make occupation, location, and age optional parameters. Use this function to create profiles for different people, 
+    demonstrating how it handles these optional parameters.
+    Example: build_profile("John", "Doe", occupation="Developer", location="USA", age=30)"""
