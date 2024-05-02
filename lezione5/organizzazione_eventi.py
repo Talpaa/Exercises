8cb8d4f59ffca74cacbc31df1fe8f2f0e18a7a54
talpaa <daniele.taccini@gmail.com>
2024-05-02 13:49:52 +0200

    lezione5 completamento esercizio organizzazione_eventi.py

--- lezione5/organizzazione_eventi.py
+++ lezione5/organizzazione_eventi.py
@@ -6,3 +6,24 @@
     Example: plan_event("Code Workshop", ["Alice", "Bob", "Charlie"],”4pm”)
     Write a function called modify_event() that accepts a dictionary, an event name, and new details to modify an already planned event.
     Example: modify_event(dictionary, "Code Workshop", ["Alice", "Bob", "Charlie"], ”4pm”)"""
+
+def plan_event(event: str, partecipanti: list, ora: str)->dict:
+
+    dictionary: dict = {
+        "Nome evento": event,
+        "Partecipanti": partecipanti,
+        "Orario": ora}
+
+    return dictionary
+
+def modify_event(dictionary: dict, event: str, partecipanti: list, ora: str)->dict:
+
+    dictionary.update({"Partecipanti": partecipanti})
+    dictionary.update({"Orario": ora})
+    return dictionary
+
+dictionary: dict = plan_event("Code Workshop", ["Alice", "Bob", "Charlie"],"4pm")
+print(dictionary)
+
+dictionary = modify_event(dictionary, "Code Workshop", ["Alice", "Bob", "Charlie","Francesco","Marco"], "6pm")
+print(dictionary)
