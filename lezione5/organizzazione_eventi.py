f6aa713b817cb80b3f210cb731ac86d5d49153b6
talpaa <daniele.taccini@gmail.com>
2024-05-02 13:33:21 +0200

    lezione5 creazione file organizzazione_eventi.py

---
+++ lezione5/organizzazione_eventi.py
@@ -0,0 +1,8 @@
+#4. Event Organizer:
+
+"""Write a function called plan_event() that accepts an event name, a list of participants, and an hour. 
+    The function should return a dictionary that includes the event name and a list of the participants. 
+    Call this function with varying numbers of participants to plan different events.
+    Example: plan_event("Code Workshop", ["Alice", "Bob", "Charlie"],”4pm”)
+    Write a function called modify_event() that accepts a dictionary, an event name, and new details to modify an already planned event.
+    Example: modify_event(dictionary, "Code Workshop", ["Alice", "Bob", "Charlie"], ”4pm”)"""
