ffa5cddb97f4fa28190f4912a99538f92eabe9bd
talpaa <daniele.taccini@gmail.com>
2024-05-07 17:14:02 +0200

    completamento esercizio 2 lezione 6

--- lezione6/lezione6_esercizi.py
+++ lezione6/lezione6_esercizi.py
@@ -1,3 +1,4 @@
+#esercizo 1
 class Person:
 
     # - name
@@ -9,6 +10,24 @@ class Person:
         self.name = name
 
         self.age = age
+
+#esercizio 2
+class Student:
+
+    # - name
+    # - age
+    # - gender
+    # - study program
+    def __init__(self, name: str, age: int, gender: str, study_program: str) -> None:
+        
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.study_program = study_program
+
+    def __str__(self) -> str:
+        
+        return f"Il nome dello studente è {self.name} ha {self.age} anni il suo gender è {self.gender} e il suo percorso di studi è {self.study_program}"
 
 
 alice = Person("Alice W.", 45)
@@ -40,3 +59,16 @@ for person in persons:
 
 
 print(youngest.name)
+
+
+
+#esercizio 2  
+print('\n\n\n')
+students: list[Student] = [Student("Mario Rossi", 24, "Male", "Fullstack"),
+                           Student("Francesco Verdi", 19, "Male", "Cloud"),
+                           Student("Sofia Proietti", 35, "Female", "Cyber Security")
+                           ]
+
+for i in students:
+
+    print(i)
