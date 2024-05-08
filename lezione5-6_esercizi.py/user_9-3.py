0ad6b27f2b8ce5c050f66ad2712f5b0629dc208b
talpaa <daniele.taccini@gmail.com>
2024-05-08 18:44:35 +0200

    completamento esercizio 9-5 lezione6

--- lezione5-6_esercizi.py/user_9-3.py
+++ lezione5-6_esercizi.py/user_9-3.py
@@ -5,17 +5,25 @@ class User:
                  last_name: str,
                  age: int,
                  cf: str,
-                 email: str):
+                 email: str,
+                 login_attempts: int = 0):
         
         self.first_name: str = first_name
         self.last_name: str = last_name
         self.age: int = age
         self.cf: str = cf
         self.email: str = email
+        self.login_attempts: int = login_attempts
+
 
     def greet_user(self):
 
         return f'\nCiao {self.first_name} {self.last_name}, come stai?'
+    
+    def increment_login_attempts(self):
+        
+        self.login_attempts += 1
+        print(self.login_attempts)
 
     def __str__(self) -> str:
         
@@ -32,10 +40,17 @@ user2 = User('Luigi', 'Verdi', 37, 'LGVRD65S67M126L', 'Luigi.Verdi@gmail.com')
 user3 = User('Rosa', 'Esposito', 54, 'RSSPST65S67M126L', 'Rosa.Esposito@gmail.com')
 
 print(user1)
-print(User.greet_user(user1))
+print(user1.greet_user())
 
 print(user2)
-print(User.greet_user(user2))
+print(user2.greet_user())
 
 print(user3)
-print(User.greet_user(user3))
+print(user3.greet_user())
+
+user1.increment_login_attempts()
+
+user2.increment_login_attempts()
+
+user3.increment_login_attempts()
+user1.increment_login_attempts()
