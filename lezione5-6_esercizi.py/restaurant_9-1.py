9031fb3dca4a6144b0b769e3860460f2d6af1b3f
talpaa <daniele.taccini@gmail.com>
2024-05-08 16:06:39 +0200

    completamento esercizio 9-1 leszione6

--- lezione5-6_esercizi.py/restaurant_9-1.py
+++ lezione5-6_esercizi.py/restaurant_9-1.py
@@ -3,11 +3,26 @@ class Restaurant:
     #-nome
     #-tipo di cunina
 
-    def __init__(self, name: str, cuisin_type) -> None:
+    def __init__(self, 
+                 name: str, 
+                 cuisin_type) -> None:
         
         self.name = name
         self.cuisin_type = cuisin_type
 
-    def describe_restaurant():
+    def describe_restaurant(self):
 
-        pass
+        print(f'\n\nRistorante\n'\
+            + f'Nome: {self.name}\n'\
+            + f'Descrizione: {self.cuisin_type}')
+        
+    def open_restaurant(self):
+
+        print(f'\nIl ristorante {self.name} è aperto')
+
+
+r1 = Restaurant(name = 'La Vecchia Roma', cuisin_type = 'Romana')
+
+Restaurant.describe_restaurant(r1)
+
+Restaurant.open_restaurant(r1)
