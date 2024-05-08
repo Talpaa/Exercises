e334836b893086f2d3109ba9034293b987dd22db
talpaa <daniele.taccini@gmail.com>
2024-05-08 15:38:59 +0200

    esercizio restaurant_9-1.py

---
+++ lezione5-6_esercizi.py/restaurant_9-1.py
@@ -0,0 +1,13 @@
+class Restaurant:
+
+    #-nome
+    #-tipo di cunina
+
+    def __init__(self, name: str, cuisin_type) -> None:
+        
+        self.name = name
+        self.cuisin_type = cuisin_type
+
+    def describe_restaurant():
+
+        pass
