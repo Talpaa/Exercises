8ddcd99119208304975ca778d9cb448634aa1e10
talpaa <daniele.taccini@gmail.com>
2024-05-10 18:05:47 +0200

    continuazione progetto zoo.py

--- gestione_zoo(mini-progetto)/zoo.py
+++ gestione_zoo(mini-progetto)/zoo.py
@@ -1,3 +1,4 @@
+#Daniele Taccini
 #controlla se una stringa può essere convertita in float
 def is_float(num):
 
@@ -7,7 +8,13 @@ def is_float(num):
     
     except ValueError:
 
-        return False
+        risposta: bool = False
+
+        if num.isdigit():
+
+            risposta = True
+
+        return risposta
 
 class Animal:
 
@@ -24,17 +31,16 @@ class Animal:
                  age: int,
                  height: float,
                  widht: float,
-                 preferred_habitat: str,
-                 healt: float) -> None:
+                 preferred_habitat: str) -> None:
         
         self.name: str = name
         self.species: str = species
 
-        while (type(age) != int)or(age < 0):
+        while (type(age) != int)or(age <= 0):
             
-            print(f'L\'età inserita non è valida')
+            print(f'\nL\'età dell\'animale inserita non è valida')
 
-            age = input(f'Perfavore inserisci un\'età che deve essere positiva ed intera o zero:     ')
+            age = input(f'Per favore inserisci un valore numerico che deve essere maggiore di zero:     ')
 
             if age.isdigit():
 
@@ -42,31 +48,36 @@ class Animal:
         else:
             self.age: int = age
 
-        while (type(height) != float)or(height <= 0):
+        while (not(is_float(height)))or(height <= 0):
 
-            height = input(f'Perfavore inserisci un\'altezza che deve essere maggiore di zero:     ')
+            print(f'\nL\'altezza dell\'animale inserita non è valida')
 
-            if is_float(height):
-
-                height = float(height)
-
-        else:
-            self.height: float = height
-
-
-        while (type(height) != float)or(height <= 0):
-
-            height = input(f'Perfavore inserisci un\'altezza che deve essere maggiore di zero:     ')
+            height = input(f'Per favore inserisci un valore numerico che deve essere maggiore di zero:     ')
 
             if is_float(height):
 
                 height = float(height)
 
         else:
-            self.widht: float = widht
+            self.height: float = float(height)
+
+
+        while (not(is_float(widht)))or(widht <= 0):
+
+            print(f'\nLa larghezza dell\'animale inserita non è valida')
+
+            widht = input(f'Per favore inserisci un valore numerico che deve essere maggiore di zero:     ')
+
+            if is_float(widht):
+
+                widht = float(widht)
+
+        else:
+            self.widht: float = float(widht)
             
         self.preferred_habitat: str = preferred_habitat
-        self.health: float = healt
+
+        self.health: float = round(100*(1/self.age), 3)
 
     def __str__(self) -> str:
         
@@ -76,7 +87,7 @@ Età: {self.age}\n\
 Altezza: {self.height}\n\
 Larghezza: {self.widht}\n\
 Habitat: {self.preferred_habitat}\n\
-Salute: {self.health}'
+Salute: {self.health}\n'
 
 
 class Fence:
@@ -90,10 +101,42 @@ class Fence:
                  habitat: str,
                  fauna: list = []) -> None:
         
-        self.area: float = area
-        self.temperature: float = temperature
+        while (not(is_float(area)))or(area <= 0):
+
+            print(f'\nL\'area del recinto inserita non è valida')
+
+            area = input(f'Per favore inserisci un valore numerico che deve essere maggiore di zero:     ')
+
+            if is_float(area):
+
+                area = float(area)
+
+        else:
+            self.area: float = float(area)
+
+        while not(is_float(temperature)):
+
+            print(f'\nLa temperatura del recinto inserita non è valida')
+
+            temperature = input(f'Per favore inserisci un valore numerico:     ')
+
+            if is_float(temperature):
+
+                temperature = float(temperature)
+
+        else:
+            self.temperature: float = float(temperature)
+
         self.habitat: str = habitat
         self.fauna: list = fauna
+
+    def __str__(self) -> str:
+        
+        return f'Dimensioni recinto: {self.area} metri quadrati\n\
+Temperatura del recinto: {self.temperature}\n\
+Habitat del recinto: {self.habitat}\n\
+Animali presenti nel recinto: {self.fauna}\n'
+
 
 class ZooKeeper:
     
@@ -176,7 +219,10 @@ class Zoo:
 
 
 
-a1: Animal =  Animal('a','b',12,12,13.00,'a',100.00)
+a1: Animal =  Animal('a', 'b', 1, 12, 14, 'c')
 
-print(type(a1.age))
 print(a1)
+
+f1: Fence = Fence(12, 10, 'a')
+
+print(f1)
