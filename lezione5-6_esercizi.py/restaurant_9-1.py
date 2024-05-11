e3cf7be380cb14a1c56100c9487b2f099e5a94be
Talpaa <daniele.taccini@gmail.com>
2024-05-11 12:38:45 +0200

    completamento esercizio 9-6  lezione6

--- lezione5-6_esercizi.py/restaurant_9-1.py
+++ lezione5-6_esercizi.py/restaurant_9-1.py
@@ -13,11 +13,7 @@ class Restaurant:
         self.cuisin_type = cuisin_type
         self.num_serv = num_serv
 
-    def describe_restaurant(self):
-
-        print(f'\n\nRistorante\n'\
-            + f'Nome: {self.name}\n'\
-            + f'Descrizione: {self.cuisin_type}')
+    
         
     def open_restaurant(self):
 
@@ -33,14 +29,39 @@ class Restaurant:
 
         print(f'Stiamo servendo il cliente numero {self.num_serv}')
 
+    def describe_restaurant(self):
+
+        print(f'\n\nRistorante\n'\
+            + f'Nome: {self.name}\n'\
+            + f'Descrizione: {self.cuisin_type}')
+
+class Ice_cream_stand(Restaurant):
+     
+    def __init__(self, 
+                  name: str, 
+                  cuisin_type, 
+                  num_serv: int = 0,
+                  flavors: list = []) -> None:
+          
+          super().__init__(name, cuisin_type, num_serv)
+
+          self.flavors = flavors
+
+    def flavors_list(self):
+         
+         print(f'\nI gusti che puoi scegliere nella gelateria {self.name} sono:\n')
+         for i in self.flavors:
+              
+              print(f'  -{i}')
+
     
 
 
-r1 = Restaurant(name = 'La Vecchia Roma', cuisin_type = 'Romana')
+r1: Restaurant = Restaurant(name = 'La Vecchia Roma', cuisin_type = 'Romana')
 
-r2 = Restaurant(name = 'Dumpling Bar', cuisin_type = 'Cinese')
+r2: Restaurant = Restaurant(name = 'Dumpling Bar', cuisin_type = 'Cinese')
 
-r3 = Restaurant(name = 'La Cucaracha', cuisin_type = 'Messicana')
+r3: Restaurant = Restaurant(name = 'La Cucaracha', cuisin_type = 'Messicana')
 
 Restaurant.describe_restaurant(r1)
 
@@ -58,3 +79,7 @@ r1.set_number_served(r1.num_serv)
 
 
 r1.set_number_served(r1.num_serv)
+
+g1: Ice_cream_stand = Ice_cream_stand(name = 'Fatamorgana', cuisin_type = 'Gelateria', flavors = ['crema', 'yougurt', 'cocco'])
+
+g1.flavors_list()
