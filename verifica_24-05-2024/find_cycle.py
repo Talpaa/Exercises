ada4445f0b01b49402732a621c304cb9af9979a5
talpaa <daniele.taccini@gmail.com>
2024-05-24 19:25:35 +0200

    a

--- verifica_24-05-2024/find_cycle.py
+++ verifica_24-05-2024/find_cycle.py
@@ -1,9 +1,31 @@
-class LinkedList:
-    
-    def __init__(self,node, next) -> None:
+class Node:
+
+    def __init__(self, node) -> None:
         
         self.node = node
-        self.next = next
+        self.next = None
+
+class LinkedList:
+    
+    def __init__(self) -> None:
+       
+       self.head: list = []
+
+    def append(self, x: int):
+
+        node: Node = Node(x)
+
+        self.head.append(node)
+
+        if node.next == None:
+
+            i = self.head.index(node)
+
+            if i-1 >= 0:
+                self.head[i-1].next = node
+
+    def get_node(self):
+        pass
         
 def has_cycle(head: Node) -> list[int]:
     pass
