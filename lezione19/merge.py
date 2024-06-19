a76284abe8beeeaca20f566ac964aa07582b56c5
Talpaa <daniele.taccini@gmail.com>
2024-06-19 12:02:25 +0200

    risoluzione errore file merge.py

--- lezione19/merge.py
+++ lezione19/merge.py
@@ -21,24 +21,50 @@ def merge(input_list_sx: list[int], input_list_dx: list[int]):
 
     for k in range(len(merge_list)):
 
-        if input_list_sx[i] > input_list_dx[j]:
+        if (input_list_sx[i] == None):
 
             merge_list[k] = input_list_dx[j]
-            j += 1
+            input_list_dx[j] = None
 
+
+            if j+1 < len(input_list_dx):
+                j += 1
+
+        elif (input_list_dx[j] == None):
+
+            merge_list[k] = input_list_sx[i]
+            input_list_sx[i] = None
+
+
+            if i+1 < len(input_list_sx):
+                i += 1
+
+        elif(input_list_sx[i] > input_list_dx[j]):
+
+            merge_list[k] = input_list_dx[j]
+            input_list_dx[j] = None
+
+
+            if j+1 < len(input_list_dx):
+                j += 1
+        
         else:
 
             merge_list[k] = input_list_sx[i]
-            i += 1
+            input_list_sx[i] = None
+
+
+            if i+1 < len(input_list_sx):
+                i += 1
 
     return merge_list
 
 
 if __name__ == '__main__':
 
-    #lista: list[int] = [31,52,856,314,22,13]
-    lista: list[int] = [0,1,2,3,4,5,6,7]
+    lista: list[int] = [31,52,856,314,22,13]
+    #lista: list[int] = [0,1,2,3,4,5,6,7]
 
-    mergeSort(lista)
+    print(mergeSort(lista))
 
 
