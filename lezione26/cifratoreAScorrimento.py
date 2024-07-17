de2ed1d4f202c3bf28071b4a68422cf6063c68cc
talpaa <daniele.taccini@gmail.com>
2024-07-17 18:22:35 +0200

    m

--- lezione26/cifratoreAScorrimento.py
+++ lezione26/cifratoreAScorrimento.py
@@ -4,41 +4,31 @@ class CifratoreAScorrimento(CodificatoreMessaggio, DecodificatoreMessaggio):
 
     def __init__(self, chiave: int) -> None:
         
+        if chiave > 25:
+            chiave = (chiave % 25)
+
         self.chiave: int = chiave
         self.__alfabeto__: list[str] = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']#0-25
 
-    def __spostaCarattere__(self, char: str)->str:
+    def __codificaCarattere__(self, char: str)->str:
         
         ind: int = self.__alfabeto__.index(char.upper())
         cn: int = ind + self.chiave
 
-        if char.isupper():
-
-
-            if cn > 25:
+        if cn > 25:
 
                 cn = (cn % 25)-1
 
-                char = self.__alfabeto__[cn]
+        if char.isupper():
 
-            else:
-
-                char = self.__alfabeto__[ind + self.chiave]
+            char = self.__alfabeto__[cn]
 
         else:
 
-            if cn > 25:
-
-                cn = (cn % 25)-1
-
-                char = self.__alfabeto__[cn].lower()
-
-            else:
-
-                char = self.__alfabeto__[ind + self.chiave].lower()
+            char = self.__alfabeto__[cn].lower()
 
         return char
-
+    
     def codifica(self, testoInChiaro: str)->str:
         
         testoCodificato: str = ''
@@ -47,16 +37,54 @@ class CifratoreAScorrimento(CodificatoreMessaggio, DecodificatoreMessaggio):
             
             if char.upper() in self.__alfabeto__:
                 
-                testoCodificato += self.__spostaCarattere__(char)
+                testoCodificato += self.__codificaCarattere__(char)
 
         return testoCodificato
     
+    def __decodificaCarattere__(self, char: str)->str:
+
+        ind: int = self.__alfabeto__.index(char.upper())
+        cn: int = ind - self.chiave
+
+        if cn < -26:
+
+            cn *= -1
+            cn %= 25 - 1
+
+        if char.isupper():
+
+            char = self.__alfabeto__[cn]
+
+        else:
+
+            char = self.__alfabeto__[cn].lower()
+
+        return char
+    
     def decodifica(self, testoCodificato: str)->str:
-        return super().decodifica()
+        
+        testoDecodificato: str = ''
+
+        for char in testoCodificato:
+            
+            if char.upper() in self.__alfabeto__:
+                
+                testoDecodificato += self.__decodificaCarattere__(char)
+
+        return testoDecodificato
     
 
-cifrario: CifratoreAScorrimento = CifratoreAScorrimento(chiave= 27)
+cifrario: CifratoreAScorrimento = CifratoreAScorrimento(chiave= 26)
 
-testoCodificato: str = cifrario.codifica(testoInChiaro='abcdefghijklmnopqrstuvwxyz')
+testo: str = 'abcdefghijklmnopqrstuvwxyz'
+print(testo)
 
+print()
+
+testoCodificato: str = cifrario.codifica(testoInChiaro=testo)
 print(testoCodificato)
+
+print()
+
+testoDecifrato: str = cifrario.decodifica(testoCodificato=testoCodificato)
+print(testoDecifrato)
